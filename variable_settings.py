68ca7a07020c19c1f8d61dc538a922e5c58e7a47
neurons4me <adammcdaniel1@gmail.com>
2019-03-06 19:17:51 -0800

    Set to prod mode to prepare for push to prod.

--- variable_settings.py
+++ variable_settings.py
@@ -13,24 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Development Settings
 #
 
-DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Production Settings
-# -Uncomment below and comment out above before pushing to prod
-
-# DEBUG = False
-# ALLOWED_HOSTS = ['adam-mcdaniel.com',
-#                  '134.209.56.22',
-#                  'localhost',
-#                  'www.adam-mcdaniel.com']
+# DEBUG = True
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 #
 # DATABASES = {
 #     'default': {
@@ -38,3 +22,19 @@ DATABASES = {
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+# Production Settings
+# -Uncomment below and comment out above before pushing to prod
+
+DEBUG = False
+ALLOWED_HOSTS = ['adam-mcdaniel.com',
+                 '134.209.56.22',
+                 'localhost',
+                 'www.adam-mcdaniel.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
